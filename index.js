8a348528b60db8eea45887205edfb3dd2935c8d2
Jamie Brown <jamiebrown201@hotmail.com>
2019-03-21 09:02:02 +0000

    add view engine set up

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const dotenv = require("dotenv").config({
   silent: process.env.NODE_ENVIRONMENT === "production"
 });
+
+const express = require("express");
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
