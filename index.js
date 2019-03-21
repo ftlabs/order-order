0fc6ffddcc603d09cecf34116cb38dc00f548633
Jamie Brown <jamiebrown201@hotmail.com>
2019-03-21 09:05:50 +0000

    initialise express

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const dotenv = require("dotenv").config({
 });
 
 const express = require("express");
+const path = require("path");
+const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
