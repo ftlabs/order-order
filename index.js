16474cf2fdb4aa9d272622ecaaca30e54529d80b
Jamie Brown <jamiebrown201@hotmail.com>
2019-03-21 09:12:28 +0000

    add port

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ const app = express();
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
+
+const PORT = process.env.PORT;
+if (!PORT) {
+  throw new Error("ERROR: PORT not specified in env");
+}
