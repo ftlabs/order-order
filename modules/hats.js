2a6e69c0fc273cc1ca3ad0d39931a2a0aaca0c49
Sqyphen <tobias.fieldhouse@gmail.com>
2019-03-25 10:25:36 +0000

    adding comments helpers

--- modules/hats.js
+++ modules/hats.js
@@ -1,32 +1,11 @@
+const comments = require("../helpers/comments");
+
 function render(req, res, data) {
-  data.commentsStructured = nestComments(data.comments);
+  const params = {
+    commentsData: data.comments
+  };
+  data.commentsStructured = comments.getNestedComments(params).data;
   res.render(data.type, data);
-}
-
-function nestComments(commentsData) {
-  const newComments = [];
-
-  commentsData.map(comment => {
-    if (comment.replyto === 0) {
-      comment.replies = findReplies(comment, commentsData);
-      newComments.push(comment);
-    }
-  });
-
-  return newComments;
-}
-
-function findReplies(comment, commentsData) {
-  let replies = [];
-
-  commentsData.map(c => {
-    if (c.replyto === comment.id) {
-      c.replies = findReplies(c, commentsData);
-      replies.push(c);
-    }
-  });
-
-  return replies;
 }
 
 module.exports = { render };
