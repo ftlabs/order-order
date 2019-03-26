3921dc80a93d4ca7d697ee80de041751f267314c
Sqyphen <tobias.fieldhouse@gmail.com>
2019-03-26 12:19:35 +0000

    adds placeholder for form submission, error reporting and status reports

--- static/js/admin/main.js
+++ static/js/admin/main.js
@@ -1,7 +1,30 @@
+const form = document.getElementById("form_new_debate");
+const btnSubmit = document.getElementById("submit_new_debate");
+const msgError = document.getElementById("msg_error");
+const msgStatus = document.getElementById("msg_status");
+
 function init() {
   addListeners();
 }
 
-function addListeners() {}
+function addListeners() {
+  btnSubmit.addEventListener("click", submitForm);
+}
+
+function submitForm(e) {
+  e.preventDefault();
+
+  console.log("submitForm");
+}
+
+function reportError(msg) {
+  msgError.innerHTML = msg;
+  msgError.classList.remove("hide");
+}
+
+function reportStatus(msg) {
+  msgStatus.innerHTML = msg;
+  msgStatus.classList.remove("hide");
+}
 
 init();
