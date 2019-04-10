afb36c020f5bd6b9819beb03d69d76251ff26b11
Jamie Brown <jamiebrown201@hotmail.com>
2019-04-10 15:58:21 +0100

    update and get methods

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+const AWS = require("aws-sdk");
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient({
+  region: "eu-west-1"
+});
+
 // view engine setup
 var hbs = require("express-hbs");
 
@@ -22,13 +28,67 @@ app.set("views", __dirname + "/views");
 
 app.use("/static", express.static(path.resolve(__dirname + "/static")));
 
+app.use(express.json());
+
 const PORT = process.env.PORT;
+
 if (!PORT) {
   throw new Error("ERROR: PORT not specified in env");
 }
 
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.post("/debate", async (req, res) => {
+  try {
+    const timestamp = new Date().getTime();
+    const params = {
+      TableName: process.env.DEBATE_TABLE,
+      Item: {
+        name: req.body.name,
+        seriesId: req.body.seriesId,
+        type: req.body.type,
+        permitted: [],
+        restricted: [],
+        specialUsers: [],
+        comments: [],
+        starter: [
+          { type: "title", text: req.body.title },
+          { type: "description", text: req.body.description }
+        ],
+        state: [],
+        teams: [],
+        votes: [],
+        createdAt: timestamp,
+        updatedAt: timestamp
+      }
+    };
+    const result = await dynamoDb.put(params).promise();
+    console.log(result);
+    res.send(JSON.stringify(result));
+  } catch (err) {
+    console.error(err);
+    res.status(404).send("Sorry can't find that!");
+  }
+});
+
+app.get("/debate", async (req, res) => {
+  console.log(req.query);
+  try {
+    const params = {
+      TableName: process.env.DEBATE_TABLE,
+      Key: {
+        name: req.query.name,
+        seriesId: req.query.seriesId
+      }
+    };
+    const result = await dynamoDb.get(params).promise();
+    res.send(JSON.stringify(result));
+  } catch (err) {
+    console.error(err);
+    res.status(404).send("Sorry can't find that!");
+  }
 });
 
 app.get("/:debateType/:debateName", (req, res) => {
