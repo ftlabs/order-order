9f4dbcf11efa1aaafceae2e23b805dfa3aa5cea2
Sqyphen <tobias.fieldhouse@gmail.com>
2019-04-15 15:40:50 +0100

    adds placeholder for dynamo calls

--- helpers/listings.js
+++ helpers/listings.js
@@ -2,6 +2,11 @@
 
 const { lstatSync, readdirSync } = require("fs");
 const { dirname, join } = require("path");
+const AWS = require("aws-sdk");
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient({
+  region: "eu-west-1"
+});
 
 function getRootDir() {
   return dirname(require.main.filename || process.mainModule.filename);
@@ -40,7 +45,25 @@ function getDebateListings(folder, searchedType = "") {
   return directoryList;
 }
 
+async function getDynamoDebateListings() {
+  /*
+  try {
+    const params = {
+      TableName: process.env.DEBATE_TABLE
+    };
+    const result = await dynamoDb.get(params).promise();
+    res.send(JSON.stringify(result));
+  } catch (err) {
+    console.error(err);
+    res.status(404).send("Sorry can't find that!");
+  }
+  */
+
+  return [];
+}
+
 module.exports = {
   getRootDir,
-  getDebateListings
+  getDebateListings,
+  getDynamoDebateListings
 };
