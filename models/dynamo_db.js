a5178d399c16bd6c6844e6fcf9d77eafb2cbc643
Sqyphen <tobias.fieldhouse@gmail.com>
2019-04-16 12:32:44 +0100

    seperating the dynamo db requests into it's own model

---
+++ models/dynamo_db.js
@@ -0,0 +1,79 @@
+const AWS = require("aws-sdk");
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient({
+  region: "eu-west-1"
+});
+
+async function getAll() {
+  let queryStatement = await query("get", {});
+
+  if (queryStatement.result) {
+    return queryStatement.result;
+  }
+
+  return { error: queryStatement.result };
+}
+
+async function getById(name, seriesId) {
+  let queryStatement = await query("get", {
+    Key: { name, seriesId }
+  });
+
+  if (queryStatement.result) {
+    return queryStatement.result;
+  }
+
+  return { error: queryStatement.result };
+}
+
+async function getAllTypes() {
+  const params = {
+    ProjectionExpression: "debateType"
+  };
+
+  let queryStatement = await query("scan", params);
+  const types = [];
+
+  if (queryStatement.result) {
+    queryStatement.result["Items"].forEach(result => {
+      if (!types.includes(result.debateType)) {
+        types.push(result.debateType);
+      }
+    });
+    return types;
+  }
+
+  return { error: queryStatement.result };
+}
+
+async function query(type, params) {
+  try {
+    let result;
+    let baseParams = {
+      TableName: process.env.DEBATE_TABLE
+    };
+
+    const allParams = Object.assign(baseParams, params);
+
+    if (type === "get") {
+      result = await dynamoDb.get(allParams).promise();
+    } else if (type === "scan") {
+      result = await dynamoDb.scan(allParams).promise();
+    }
+
+    if (result.hasOwnProperty("Items")) {
+      return { result };
+    } else {
+      return { result: [] };
+    }
+  } catch (err) {
+    console.error(err);
+    return `Error with request ${err}`;
+  }
+}
+
+module.exports = {
+  getAll,
+  getById,
+  getAllTypes
+};
