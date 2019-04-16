a70d72ae1e712dc046301e7edb2b523ee8c9c63a
Sqyphen <tobias.fieldhouse@gmail.com>
2019-04-16 19:27:02 +0100

    adds placeholder for editing a debate

--- routes/admin.js
+++ routes/admin.js
@@ -21,6 +21,21 @@ router.get("/create_debate", (req, res) => {
   res.render("admin/create_debate");
 });
 
+router.get("/edit_debate", async (req, res) => {
+  const username =
+    req.cookies.s3o_username !== undefined ? req.cookies.s3o_username : null;
+  try {
+    let debateList = await dynamo_db.getAllDebateLists();
+    res.render("admin/edit_debate", {
+      username: username,
+      debateList: debateList
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(404).send("Sorry can't find that!");
+  }
+});
+
 router.get("/moderation", (req, res) => {
   res.render("admin/moderation");
 });
