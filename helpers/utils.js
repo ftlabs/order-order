50337aa07e0ad6bd2774e5a5826eb5b21063ba79
Lily2point0 <lily.madar@ft.com>
2019-04-30 14:32:21 +0100

    style homepage (desktop only)

---
+++ helpers/utils.js
@@ -0,0 +1,26 @@
+function formatDate(timestamp) {
+	const months = ["January", "February", "March", "April", "June", "July", "August", "September", "October", "November", "December"];
+	const date = new Date(timestamp);
+
+	const format = {
+		datetime: `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${(date.getDate()).toString().padStart(2, '0')}`,
+		readable: `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`
+	}
+
+	return format;
+}
+
+function sortByDate(arr, property) {
+	return arr.sort((a,b) => {
+		if(a[property] === b[property]) {
+			return 0;
+		}
+
+		return a[property] > b[property]? -1 : 1;
+	});
+}
+
+module.exports = {
+	formatDate,
+	sortByDate
+}
