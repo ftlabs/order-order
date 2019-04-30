7883c6cbd286e8f51cfde619bc0f5e82275ca932
Sqyphen <tobias.fieldhouse@gmail.com>
2019-04-30 11:53:38 +0100

    updates comment display feature

--- modules/structured.js
+++ modules/structured.js
@@ -1,43 +1,53 @@
 const commentHelper = require('../helpers/comments');
 
 function display(req, res, data) {
-  let commentsFor = [];
-  let commentsAgainst = [];
   const { debate, user } = data;
-  const { id, title, description, debateStatus, debateType } = debate;
-
-  if (debate.comments) {
-    commentsFor = debate.comments.filter(comment => {
-      if (comment.tags.includes('for')) {
-        return comment;
-      }
-    });
-    commentsAgainst = debate.comments.filter(comment => {
-      if (comment.tags.includes('against')) {
-        return comment;
-      }
-    });
-  }
+  const { id, title, description, debateStatus, debateType, comments } = debate;
+  const { commentsFor, commentsAgainst } = getAndNestComments(comments);
 
   debateOpen = debateStatus === 'open' ? true : false;
-
-  commentsForStructured = commentHelper.getNestedComments({
-    commentsData: commentsFor,
-  }).data;
-
-  commentsAgainstStructured = commentHelper.getNestedComments({
-    commentsData: commentsAgainst,
-  }).data;
 
   res.render(debateType, {
     id,
     title,
     description,
     debateOpen,
-    commentsForStructured,
-    commentsAgainstStructured,
+    commentsFor,
+    commentsAgainst,
     user,
   });
+}
+
+function getAndNestComments(comments) {
+  let commentsFor = [];
+  let commentsAgainst = [];
+
+  if (comments) {
+    commentsFor = comments.filter(comment => {
+      if (comment.tags.includes('for')) {
+        return comment;
+      }
+    });
+    commentsAgainst = comments.filter(comment => {
+      if (comment.tags.includes('against')) {
+        return comment;
+      }
+    });
+
+    // adds nesting structure
+    commentsFor = commentHelper.getNestedComments({
+      commentsData: commentsFor,
+    });
+
+    commentsAgainst = commentHelper.getNestedComments({
+      commentsData: commentsAgainst,
+    });
+  }
+
+  return {
+    commentsFor: commentsFor,
+    commentsAgainst: commentsAgainst,
+  };
 }
 
 module.exports = { display };
