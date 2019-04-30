abf1005f16c2184123ec59976ae9b19123a288e9
Jamie Brown <jamiebrown201@hotmail.com>
2019-04-30 15:32:59 +0100

    extract out validaton

--- routes/rating.js
+++ routes/rating.js
@@ -16,21 +16,29 @@ router.post('/:debateId', async (req, res) => {
         }),
       ],
     };
-    const debateData = await dynamoDb.getById(debateId);
-    const commentData = debateData.Items[0].comments[index];
-    if (
-      commentData.ratings.find(
-        rating => rating.user === req.cookies.s3o_username,
-      )
-    ) {
+    if (await invalidPost(debateId, req.cookies.s3o_username, index)) {
       throw new Error('You have already rated this comment');
     }
-    const result = await dynamoDb.updateDebate(debateId, data);
+    await dynamoDb.updateDebate(debateId, data);
     res.redirect(backURL);
   } catch (err) {
     console.error(err);
     res.status(404).send("Sorry can't find that!");
   }
 });
+
+async function invalidPost(debateId, username, index) {
+  try {
+    const debateData = await dynamoDb.getById(debateId);
+    const commentData = debateData.Items[0].comments[index];
+    if (commentData.ratings.find(rating => rating.user === username)) {
+      return true;
+    }
+    return false;
+  } catch (err) {
+    console.error(err);
+    return true;
+  }
+}
 
 module.exports = router;
