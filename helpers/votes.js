4888f087e8f6bca3630ea47b66c62f017ae3caf1
Sqyphen <tobias.fieldhouse@gmail.com>
2019-05-08 16:34:46 +0100

    adds voting results display

--- helpers/votes.js
+++ helpers/votes.js
@@ -10,6 +10,36 @@ function hasVoted(votes, username) {
   return voted;
 }
 
+function calculateResults(votes, options) {
+  let voteCalculations = [];
+  let voteResults = [];
+  let voteTotal = 0;
+
+  options.forEach(opt => {
+    voteCalculations[opt] = {
+      name: opt,
+      number: 0,
+      percentage: 0,
+    };
+  });
+
+  votes.forEach(vote => {
+    if (options.includes(vote.content[0])) {
+      voteTotal = voteTotal + 1;
+      voteCalculations[vote.content[0]].number =
+        voteCalculations[vote.content[0]].number + 1;
+      voteCalculations[vote.content[0]].percentage =
+        (voteCalculations[vote.content[0]].number / voteTotal) * 100;
+    }
+  });
+
+  options.forEach(opt => {
+    voteResults.push(voteCalculations[opt]);
+  });
+  return voteResults;
+}
+
 module.exports = {
   hasVoted,
+  calculateResults,
 };
