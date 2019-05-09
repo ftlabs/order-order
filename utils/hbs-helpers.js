a20fd6664bab1345ca769045f233811babbe6e6d
Sqyphen <tobias.fieldhouse@gmail.com>
2019-05-09 15:17:00 +0100

    adds messaging if there are no comments yet

--- utils/hbs-helpers.js
+++ utils/hbs-helpers.js
@@ -2,8 +2,22 @@ function ifEquals(arg1, arg2, options) {
   return arg1 === arg2 ? options.fn(this) : options.inverse(this);
 }
 
+function ifThisOrThat(arg1, arg2, options) {
+  return arg1.length > 0 || arg2.length > 0
+    ? options.fn(this)
+    : options.inverse(this);
+}
+
+function ifNotThisOrThat(arg1, arg2, options) {
+  return arg1.length > 0 || arg2.length > 0
+    ? options.inverse(this)
+    : options.fn(this);
+}
+
 function registerHelpers(hbs) {
   hbs.registerHelper('ifEquals', ifEquals);
+  hbs.registerHelper('ifThisOrThat', ifThisOrThat);
+  hbs.registerHelper('ifNotThisOrThat', ifNotThisOrThat);
 }
 
 module.exports = {
