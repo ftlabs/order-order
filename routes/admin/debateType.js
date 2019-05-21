f99705d91b42832858f9229f52e5794a2dcbc052
Jamie Brown <jamiebrown201@hotmail.com>
2019-05-21 13:56:13 +0100

    add error handling

--- routes/admin/debateType.js
+++ routes/admin/debateType.js
@@ -6,10 +6,16 @@ const { getS3oUsername } = require('../../helpers/cookies');
 
 router.get('/create', (req, res) => {
   const username = getS3oUsername(req.cookies);
+  const { alertType, alertAction } = req.query;
 
   res.render('admin/createDebateType', {
     username,
     page: 'create-type',
+    alertMessage: getAlertMessage(
+      alertType,
+      alertAction ? alertAction : 'creating',
+    ),
+    alertType,
   });
 });
 
@@ -34,26 +40,26 @@ router.post('/create', async (req, res) => {
       description,
       displayName,
     });
-    const username = getS3oUsername(req.cookies);
-    res.render('admin/createDebateType', {
-      username,
-      success: true,
-      page: 'create-type',
-    });
+    res.redirect(
+      `/admin/debate_type/edit/${name}?alertType=success&alertAction=creating`,
+    );
   } catch (err) {
     console.error(err);
+    res.redirect(
+      `/admin/debate_type/create?alertType=error&alertAction=creating`,
+    );
   }
 });
 
 router.get('/edit/:debateName', async (req, res) => {
   try {
+    const { alertType, alertAction } = req.query;
     const username = getS3oUsername(req.cookies);
     const { debateName } = req.params;
     const debateType = await dynamoDb.getDebateType(debateName);
     if (debateType.Items.length === 0) {
       throw new Error('Cant find debate type');
     }
-
     const {
       description,
       name,
@@ -71,6 +77,11 @@ router.get('/edit/:debateName', async (req, res) => {
         index,
       })),
       page: 'edit-type',
+      alertMessage: getAlertMessage(
+        alertType,
+        alertAction ? alertAction : 'editing',
+      ),
+      alertType,
     });
   } catch (err) {
     console.error(err);
@@ -80,5 +91,16 @@ router.get('/edit/:debateName', async (req, res) => {
     });
   }
 });
+
+function getAlertMessage(alertType, action) {
+  switch (alertType) {
+    case 'success':
+      return `${action} your debate type was succesful.`;
+    case 'error':
+      return `Something went wrong ${action} your debate type.`;
+    default:
+      return 'Something went wrong.';
+  }
+}
 
 module.exports = router;
