d284ec6f99a1987d97895dc85b11514c4a1cf634
Sqyphen <tobias.fieldhouse@gmail.com>
2019-05-23 14:19:05 +0100

    spacing issues solution

--- static/js/debates/structured/main.js
+++ static/js/debates/structured/main.js
@@ -1,43 +1,43 @@
 /* eslint-env browser */
 
-/* eslint-env browser */
-
 function replyToComment(commentId) {
-  const replyTo = document.querySelector('.comment-reply-to');
-  const commentReplyNotification = document.querySelector(
-    '.comment-reply-notification',
-  );
-  const commentReplyMessage = document.querySelector('.comment-reply-message');
+	const replyTo = document.querySelector('.comment-reply-to');
+	const commentReplyNotification = document.querySelector(
+		'.comment-reply-notification'
+	);
+	const commentReplyMessage = document.querySelector(
+		'.comment-reply-message'
+	);
 
-  replyTo.setAttribute('value', commentId);
-  commentReplyNotification.classList.remove('hidden');
-  commentReplyMessage.innerHTML = 'Replying to comment ' + commentId;
+	replyTo.setAttribute('value', commentId);
+	commentReplyNotification.classList.remove('hidden');
+	commentReplyMessage.innerHTML = 'Replying to comment ' + commentId;
 }
 
 function removeReplyToComment() {
-  const replyTo = document.querySelector('.comment-reply-to');
-  const commentReplyNotification = document.querySelector(
-    '.comment-reply-notification',
-  );
-  commentReplyNotification.classList.add('hidden');
-  replyTo.removeAttribute('value');
+	const replyTo = document.querySelector('.comment-reply-to');
+	const commentReplyNotification = document.querySelector(
+		'.comment-reply-notification'
+	);
+	commentReplyNotification.classList.add('hidden');
+	replyTo.removeAttribute('value');
 }
 
 function addReplyEventListeners() {
-  const replyLinks = document.querySelectorAll('.reply');
-  const commentReplyRemove = document.querySelector('.comment-reply-remove');
+	const replyLinks = document.querySelectorAll('.reply');
+	const commentReplyRemove = document.querySelector('.comment-reply-remove');
 
-  Array.from(replyLinks).forEach(element =>
-    element.addEventListener('click', () =>
-      replyToComment(element.getAttribute('data-comment-id')),
-    ),
-  );
+	Array.from(replyLinks).forEach((element) =>
+		element.addEventListener('click', () =>
+			replyToComment(element.getAttribute('data-comment-id'))
+		)
+	);
 
-  commentReplyRemove.addEventListener('click', removeReplyToComment);
+	commentReplyRemove.addEventListener('click', removeReplyToComment);
 }
 
 function init() {
-  addReplyEventListeners();
+	addReplyEventListeners();
 }
 
 document.addEventListener('DOMContentLoaded', init);
