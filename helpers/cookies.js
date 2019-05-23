2c6b60475b22d5753651324c1d85c39bb23427fe
Sqyphen <tobias.fieldhouse@gmail.com>
2019-05-23 14:49:29 +0100

    adds a few straggler files

--- helpers/cookies.js
+++ helpers/cookies.js
@@ -1,17 +1,17 @@
 function getProperty(cookie, name) {
-  if (cookie && Object.prototype.hasOwnProperty.call(cookie, name)) {
-    const propertyName = cookie[name];
-    if (propertyName) {
-      return propertyName;
-    }
-  }
-  return null;
+	if (cookie && Object.prototype.hasOwnProperty.call(cookie, name)) {
+		const propertyName = cookie[name];
+		if (propertyName) {
+			return propertyName;
+		}
+	}
+	return null;
 }
 
 function getS3oUsername(cookie) {
-  return getProperty(cookie, 's3o_username');
+	return getProperty(cookie, 's3o_username');
 }
 
 module.exports = {
-  getS3oUsername,
+	getS3oUsername
 };
